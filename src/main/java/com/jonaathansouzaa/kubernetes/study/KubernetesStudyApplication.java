3d2e5376bcbeafb75c8de2a6cb93fcd230f5b09f
Jonathan Souza <jsouzajava@gmail.com>
2023-03-05 11:58:24 -0300

    first commit

---
+++ src/main/java/com/jonaathansouzaa/kubernetes/study/KubernetesStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jonaathansouzaa.kubernetes.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonaathansouzaa/kubernetes/study/KubernetesStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonaathansouzaa.kubernetes.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
